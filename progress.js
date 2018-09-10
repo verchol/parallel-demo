bde4ef393fb43ef90281a9d086729383ca267b6c
verchol <verchol@gmail.com>
2018-09-11 00:33:42 +0300

    fix

--- progress.js
+++ progress.js
@@ -4,8 +4,8 @@ const ProgressBar = require('progress');
 
 const progress = (_.toNumber(process.env.PROGRESS) || 80)%101
 console.log(`the progress is ${progress}`);
-
-var bar = new ProgressBar(':bar', { total: progress });
+const settings = process.env.PROGRESS_SETTINGS || ":bar"
+var bar = new ProgressBar(settings, { total: progress });
 var timer = setInterval(function () {
   bar.tick();
   if (bar.complete) {
